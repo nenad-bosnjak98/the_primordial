f2311c5d6529d947ac78e0c1226865035fcfa6a0
mysunandmoon <57789512+nenad-bosnjak98@users.noreply.github.com>
2022-09-10 00:34:36 +0200

    Jump Functionality
    Added jumping to main character. Created a system where the space button can't be spammed. The ground needs to be hit before the next jump can happen.

--- The Primordial/Assets/Scripts/Player.cs
+++ The Primordial/Assets/Scripts/Player.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
+
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    private float jumpForce = 5.0f;
+    private bool resetJump = false;
+
+    private bool onGround = false;
+    
     
     void Start()
     {
@@ -15,7 +24,33 @@ public class Player : MonoBehaviour
     void Update()
     {
         float moving = Input.GetAxisRaw("Horizontal"); // Raw values instead of increasing and decreasing ones
+        
+
+        if (Input.GetKeyDown(KeyCode.Space) && onGround == true)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+            onGround = false;
+            resetJump = true;
+            StartCoroutine(ResetJumpRoutine());
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.65f, 1 << 8);
+        Debug.DrawRay(transform.position, Vector2.down * 0.65f, Color.green);
+        if(hit.collider != null)
+        {
+            if(resetJump == false)
+            {
+                onGround = true;
+            }
+            
+        }
 
         rigidBody.velocity = new Vector2(moving, rigidBody.velocity.y);
+    }
+
+    IEnumerator ResetJumpRoutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        resetJump = false;
     }
 }
