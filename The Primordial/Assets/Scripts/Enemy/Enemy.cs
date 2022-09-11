44b21a4bdcad68637ee1167bb303a0e0b4c6a045
mysunandmoon <57789512+nenad-bosnjak98@users.noreply.github.com>
2022-09-11 17:10:02 +0200

    Shared Behavior for Enemy Class
    Both the Heavy and Light Bandit classes were exactly the same in terms of implementation, so edited main functionality inside Enemy.cs file. Added a couple of virtual methods for individual class edits.

--- The Primordial/Assets/Scripts/Enemy/Enemy.cs
+++ The Primordial/Assets/Scripts/Enemy/Enemy.cs
@@ -11,10 +11,60 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField]
     protected Transform pointOne, pointTwo;
 
-    public virtual void Attack()
-    {
+    protected Vector3 currentTargetPosition;
+    protected Animator animator;
+    protected SpriteRenderer spriteRenderer;
 
+    public virtual void Init()
+    {
+        animator = GetComponentInChildren<Animator>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
-    public abstract void Update();
+    private void Start()
+    {
+        Init();
+    }
+
+    public virtual void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+
+        FlipWhenStopped();
+        Movement();
+        
+    }
+
+    public virtual void Movement()
+    {
+        if (transform.position == pointOne.position)
+        {
+            currentTargetPosition = pointTwo.position;
+            animator.SetTrigger("Idle");
+        }
+        else if (transform.position == pointTwo.position)
+        {
+            currentTargetPosition = pointOne.position;
+            animator.SetTrigger("Idle");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, currentTargetPosition, speed * Time.deltaTime);
+    }
+
+    public virtual void FlipWhenStopped()
+    {
+        if (currentTargetPosition == pointOne.position)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    
 }
--- The Primordial/Assets/Scripts/Enemy/HeavyBandit.cs
+++ The Primordial/Assets/Scripts/Enemy/HeavyBandit.cs
@@ -4,54 +4,12 @@ using UnityEngine;
 
 public class HeavyBandit : Enemy
 {
-    private Vector3 currentTargetPosition;
-    private Animator anim;
-    private SpriteRenderer heavyBanditSprite;
 
-    public void Start()
+    public override void Init()
     {
-        anim = GetComponentInChildren<Animator>();
-        heavyBanditSprite = GetComponentInChildren<SpriteRenderer>();
-    }
-
-    public override void Update()
-    {
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Combat Idle"))
-        {
-            return;
-        }
-
-        FlipWhenStopped();
-        Movement();
-
+        base.Init();
     }
 
 
-    void Movement()
-    {
-        if (transform.position == pointOne.position)
-        {
-            currentTargetPosition = pointTwo.position;
-            anim.SetTrigger("Idle");
-        }
-        else if (transform.position == pointTwo.position)
-        {
-            currentTargetPosition = pointOne.position;
-            anim.SetTrigger("Idle");
-        }
 
-        transform.position = Vector3.MoveTowards(transform.position, currentTargetPosition, speed * Time.deltaTime);
-    }
-
-    void FlipWhenStopped()
-    {
-        if (currentTargetPosition == pointOne.position)
-        {
-            heavyBanditSprite.flipX = true;
-        }
-        else
-        {
-            heavyBanditSprite.flipX = false;
-        }
-    }
 }
